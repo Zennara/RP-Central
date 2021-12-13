d4614319ed345bdc4d9b5c54efa60f38ee3c3524
Zennara <60081399+sceaganlondfred@users.noreply.github.com>
2021-12-13 04:19:58 +0000

    setup status

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ import os
 import asyncio
 from discord import Embed
 from discord import Webhook, AsyncWebhookAdapter
+from replit import db
 
 #declare client
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
-
+@client.event
+async def on_ready():
+  print("\nRP Central Ready\n")
+  await client.change_presence(activity=discord.Game(name="a RP experience"))
 
 keep_alive.keep_alive() 
 #keep the bot running after the window closes, use UptimeRobot to ping the website at least every <60min. to prevent the website from going to sleep, turning off the bot
