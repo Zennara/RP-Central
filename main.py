70fcdb33c61272a97463ffdcd372804d53c2d12b
Zennara <60081399+sceaganlondfred@users.noreply.github.com>
2021-12-13 05:02:34 +0000

    setup db

--- main.py
+++ main.py
@@ -18,6 +18,20 @@ async def on_ready():
   print("\nRP Central Ready\n")
   await client.change_presence(activity=discord.Game(name="a RP experience"))
 
+prefix = "z/"
+
+@client.event
+async def on_guild_join(guild):
+  if db[guild.id] not in db.keys():
+    db[guild.id] = {"prefix" : "z/", "role": "", "accounts":{}}
+
+@client.event
+async def on_message(message):
+  messagecontent = message.content.lower()
+
+  if messagecontent == prefix + "create":
+
+
 keep_alive.keep_alive() 
 #keep the bot running after the window closes, use UptimeRobot to ping the website at least every <60min. to prevent the website from going to sleep, turning off the bot
 
