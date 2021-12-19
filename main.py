04bd1bc6812e9a7d63e28243ffb2e4bd400c2730
Keagan Landfried <64995253+Zennara@users.noreply.github.com>
2021-12-19 23:43:54 +0000

    comment

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from replit import db
 import json
 import requests
 
+#api limit checker
 r = requests.head(url="https://discord.com/api/v1")
 try:
     print(f"Rate limit {int(r.headers['Retry-After']) / 60} minutes left")
