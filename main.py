6e87904b5a63f075dbc210cb8ca91d5709cfaa2e
Keagan Landfried <64995253+Zennara@users.noreply.github.com>
2021-12-19 21:50:07 +0000

    Added rate limit checker

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ from replit import db
 import json
 import requests
 
+r = requests.head(url="https://discord.com/api/v1")
+try:
+    print(f"Rate limit {int(r.headers['Retry-After']) / 60} minutes left")
+except:
+    print("No rate limit")
+
 #declare client
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
